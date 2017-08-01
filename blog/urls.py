dbd6cedae00cdaad3fbd2f392e9e5675e6012456
Arnaud wat <arnoWD@hotmail.fr>
2017-08-01 18:15:23 +0200

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^accueil$', views.home),
+    url(r'^article/(?P<year>\d{4})/(?P<month>\d{2})$', views.list_articles, name = 'list_articles'),
+    url(r'^redirection$', views.view_redirection, name = 'view_redirection'),
+    url(r'^date$', views.date_actuelle, name='date_actuelle'),
+    url(r'^addition/(?P<nombre1>\d+)/(?P<nombre2>\d+)/$', views.addition),
+]
---
+++ blog/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from datetime import datetime
+
+def home(request):
+    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
+    text = """<h1>Bienvenue sur mon blog !</h1>
+              <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>"""
+    return HttpResponse(text)
+
+def list_articles(request, month, year):
+    """ Liste des articles d'un mois précis. """
+    if int(month) > 12:
+    	raise Http404
+    return HttpResponse(
+        "Vous avez demandé les articles de {0} {1}.".format(month, year)  
+        
+    ) 
+
+def view_article(request, id_article):
+    """ 
+    Vue qui affiche un article selon son identifiant (ou ID, ici un numéro)
+    Son ID est le second paramètre de la fonction (pour rappel, le premier
+    paramètre est TOUJOURS la requête de l'utilisateur)
+    """
+    return HttpResponse(
+        "Vous avez demandé l'article #{0} !".format(id_article)    
+    )
+
+def view_redirection(request):
+    return HttpResponse("Vous avez été redirigé.")
+
+def date_actuelle(request):
+	couleurs = ['rouge', 'orange', 'jaune', 'vert', 'bleu', 'indigo', 'violet']
+	date = datetime.now()
+	return render(request, 'blog/date.html', locals())
+
+def addition(request, nombre1, nombre2):
+	total = int(nombre1) + int(nombre2)
+	return render(request, 'blog/addition.html', locals())
+
